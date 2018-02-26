c70a076e9d4ccdf6eff9a5b5c2664b33a7415653
VLanc <vlancv@gmail.com>
2018-02-27 01:25:29 +0300

    Quadratic equation task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let valuesArr = equation.replace(/ /g, "").replace("^2", "").split("*x"); // an array of a, b, c
+  let a = +valuesArr[0];
+      b = +valuesArr[1];
+      c = +valuesArr[2];
+
+  let discriminant = (b * b) - 4 * a * c;
+  let x1 = Math.round((-b + Math.sqrt(discriminant)) / (2 * a)),
+      x2 = Math.round((-b - Math.sqrt(discriminant)) / (2 * a));
+
+  let solutions = [x1, x2].sort((a, b) => a - b);
+
+  return solutions;
 }
